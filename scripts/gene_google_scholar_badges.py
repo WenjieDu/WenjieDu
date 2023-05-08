a14ff5f10c4cd4b345f82de648c3bb62e563b545
Wenjie Du <wenjay.du@gmail.com>
2023-05-08 22:51:14 +0800

    feat: update the script generating citation badges;

--- scripts/gene_google_scholar_badges.py
+++ scripts/gene_google_scholar_badges.py
@@ -1,5 +1,17 @@
 """
-This script utilize the package GSBG to generate citation badges for my profile and articles on Google Scholar.
+Hey, congrats! You find this script to help you generate your citation badges!
+You've almost done the job. Just read and follow the instructions below.
+
+This script helps generate citation badges for profiles and articles on Google Scholar.
+It works with the GitHub workflow ../.github/workflows/gene_citation_badges.yml
+to run and update the badges automatically under the directory ../figs/citation_badges.
+Therefore, you don't need to run this script manually. All the steps you need to do are:
+
+1. fork this repository to your own GitHub account;
+2. replace the links in the dictionary `link_dict` below with your own links;
+3. keep this script, the workflow yml, and the directory ../figs/citation_badges. Others can be removed;
+4. please star this repository and GSBG https://github.com/WenjieDu/GSBG if you find them useful. Thanks!
+
 """
 
 # Created by Wenjie Du <wenjay.du@gmail.com>
@@ -12,17 +24,25 @@ import gsbg
 badge_saving_path = "figs/citation_badges"
 
 link_dict = {
+
+    # "name_of_this_svg_to_be_saved": {
+    #     "link_type": "profile",  # the type of this link, can be "profile" or "article"
+    #     "link": "past your link here",  # the link of GoogleScholar item to be parsed
+    # },
+
+    # belows are my examples
+
     "wdu": {
-        "link": "https://scholar.google.com/citations?user=j9qvUg0AAAAJ&hl=en",
         "link_type": "profile",
+        "link": "https://scholar.google.com/citations?user=j9qvUg0AAAAJ&hl=en",
     },
     "saits": {
-        "link": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=j9qvUg0AAAAJ&citation_for_view=j9qvUg0AAAAJ:Y0pCki6q_DkC",
         "link_type": "article",
+        "link": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=j9qvUg0AAAAJ&citation_for_view=j9qvUg0AAAAJ:Y0pCki6q_DkC",
     },
     "jocn2021": {
-        "link": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=j9qvUg0AAAAJ&citation_for_view=j9qvUg0AAAAJ:zYLM7Y9cAGgC",
         "link_type": "article",
+        "link": "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=j9qvUg0AAAAJ&citation_for_view=j9qvUg0AAAAJ:zYLM7Y9cAGgC",
     },
 }
 
